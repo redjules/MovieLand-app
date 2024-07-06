ace3314e2dc1eefd089dc3c9bea03de0866dd104
Ana Pedra <106017493+redjules@users.noreply.github.com>
2024-07-06 12:18:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,7 @@ import SearchIcon from './search.svg';
 
 const API_URL = 'http://www.omdbapi.com?apikey=39e9cff0';
 
-const movie1 = {
-    "Title": "Amazing Spiderman Syndrome",
-    "Year": "2012",
-    "imdbID": "tt2586634",
-    "Type": "movie",
-    "Poster": "N/A"
-}
+
 const App = () => {
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
