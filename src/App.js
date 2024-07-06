2e09ebd02d33ed33ec72af56f2533d499a6ccff8
Ana Pedra <106017493+redjules@users.noreply.github.com>
2024-07-06 12:51:49 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import { useState, useEffect } from 'react';
-import MovieCard from './MovieCard';
-import './App.css';
-import SearchIcon from './search.svg';
+import React, { useState, useEffect } from "react";
+
+import MovieCard from "./MovieCard";
+import SearchIcon from "./search.svg";
+import "./App.css";
 
 const API_URL = 'http://www.omdbapi.com?apikey=39e9cff0';
 
 
 const App = () => {
+    const [searchTerm, setSearchTerm] = useState("");
     const [movies, setMovies] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
+    
 
    useEffect(() => {
-      searchMovies('Batman');
-        
+      searchMovies("Batman");
     }, []);
   
   const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
+        
         setMovies(data.Search);
   };
 
@@ -42,7 +44,7 @@ const App = () => {
       {movies?.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
-            <MovieCard movie={movie}/>
+            <MovieCard movie={movie} />
            ))}
         </div>
       ) :  (
